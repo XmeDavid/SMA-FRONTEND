7abcf194c99c60cbfce492d18fd098366fcd1a27
David <xmedavid@gmail.com>
2022-04-19 23:27:31 +0100

    Login Page

---
+++ lib/consts.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const firstColor = Color.fromRGBO(38, 150, 255, 1);
+const firstColor7 = Color.fromRGBO(27, 105, 179, 1);
+const firstColor5 = Color.fromRGBO(19, 75, 128, 1);
+const firstColor3 = Color.fromRGBO(11, 45, 77, 1);
+const secondColor = Color.fromRGBO(140, 177, 255, 1);
+const secondColor7 = Color.fromRGBO(98, 124, 179, 1);
+const secondColor5 = Color.fromRGBO(70, 88, 128, 1);
+const secondColor3 = Color.fromRGBO(42, 52, 77, 1);
+const thirdColor = Color.fromRGBO(240,242, 255, 1);
+const thirdColor7 = Color.fromRGBO(168,169, 179, 1);
+const thirdColor5 = Color.fromRGBO(120,121, 128, 1);
+const thirdColor3 = Color.fromRGBO(72,73, 77, 1);
+const bgColor = Color.fromRGBO(34,36,51,1);
+const black10p = Color.fromRGBO(0, 0, 0, 0.1);
+
+const smallPadding = 8.0;
+const defaultPadding = 16.0;
+const ExtraPadding = 32.0;
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'consts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SMA());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SMA extends StatelessWidget {
+  const SMA({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'SMA - Support Management Application',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: bgColor,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key,}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginPage> createState() => _MyLoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _MyLoginPageState extends State<LoginPage> {
+  /**
+   * Search later, something to do with the state
+   * Might be usefull to define the user state after login idk
+   *
+  int _stateVariable = 0;
+  void _loginFunction() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _stateVariable++;
     });
   }
+  */
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: LoginContainer(),
+        ),
+      )
+    );
+
+  }
+}
+
+class LoginContainer extends StatelessWidget{
+  const LoginContainer({Key? key,}): super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding),
+      decoration: const BoxDecoration(
+        color: secondColor3,
+        borderRadius: BorderRadius.all(Radius.circular(20))
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      height: 364,
+      width: 512,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(defaultPadding),
+            child: Text("Login",style: Theme.of(context).textTheme.titleLarge,),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(defaultPadding),
+            child: InputField(hintText: "Username", isPassword: false,),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(defaultPadding),
+            child: InputField(hintText: "Password",isPassword: true,),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(defaultPadding),
+            child: Center(
+              widthFactor: 10,
+              heightFactor: 1,
+              child: TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(secondColor3),
+                  backgroundColor: MaterialStateProperty.all(firstColor),
+                  fixedSize: MaterialStateProperty.all(Size(160,60)),
+                ),
+                child: Text("Login",style: Theme.of(context).textTheme.titleLarge,),
+                onPressed: (){},
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  const InputField({
+    Key? key, required this.hintText, required this.isPassword
+  }) : super(key: key);
+
+  final String hintText;
+  final bool isPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        hintText: hintText,
+        fillColor: black10p,
+        filled: true,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
