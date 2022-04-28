df7464000334978b1e2cb69abc3863268e4b6184
David <xmedavid@gmail.com>
2022-04-28 19:45:58 +0100

    Progress on Create Ticket Page

--- lib/screens/new_ticket_screen.dart
+++ lib/screens/new_ticket_screen.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sma_frontend/consts.dart';
-import 'package:sma_frontend/screens/side_menu.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 import '../responsive.dart';
+import '../consts.dart';
+import 'side_menu.dart';
 
 class NewTicketScreen extends StatefulWidget {
   const NewTicketScreen({Key? key}) : super(key: key);
@@ -17,6 +20,24 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
 
   final ticketTitleController = TextEditingController();
   final ticketDescriptionController = TextEditingController();
+
+  final sampleCategories = ["I&T Support", "Warranty", "Client Request", "Help Desk support"];
+  final sampleContracts = [
+    "Contract #0001 - Blue Dev Entreprises",
+    "Contract #0002 - inCentea",
+    "Contract #0003 - Inforabreu",
+    "Contract #0004 - Dev Brain"
+  ];
+  final assets = ["Computer #1", " Computer #2", "Computer #3", "Printer #1", "Printer #2"];
+
+  String _selectedCategory = "";
+  String _selectedContract = "";
+  String _selectedClient = "";
+  List<String>? selectedAssets;
+
+  bool isClient(){
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,28 +63,76 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
                     borderRadius: BorderRadius.all(Radius.circular(20))
                   ),
                   height: MediaQuery.of(context).size.height * 0.8,
-                  width: MediaQuery.of(context).size.width * (Responsive.isDesktop(context) ? 0.666 : 0.8),
-                  child: Center(
+                  width: MediaQuery.of(context).size.width * (Responsive.isDesktop(context) ? 0.666 : 0.9),
                     child: Form(
                       key: _formKey,
-                      child: Column(
-                        children: [
-                          TextField(
-                            labelText: "Title",
-                            hintText: "Ticket Title",
-                            controller: ticketTitleController,
-                            size: MediaQuery.of(context).size.width * (Responsive.isDesktop(context) ? 0.666 : 0.8) - 98,
-                          ),
-                          TextBox(
-                              labelText: "Description",
-                              hintText: "Ticket Description",
-                              controller: ticketDescriptionController,
-                              size: 400)
-                        ],
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            if(!isClient())DropDown(this, label: "Client",items: const ["sampleCategories","Client #1", "Client #2"],callback: (s) =>{
+                              _selectedClient = s
+                              },
+                            ),
+                            TextField(
+                              labelText: "Title",
+                              hintText: "Ticket Title",
+                              controller: ticketTitleController,
+                              size: MediaQuery.of(context).size.width * (Responsive.isDesktop(context) ? 0.666 : 0.9) - 98,
+                            ),
+                            TextBox(
+                                labelText: "Description",
+                                hintText: "Ticket Description",
+                                controller: ticketDescriptionController,
+                                size: 400),
+                            DropDown(this, label: "Category",items: sampleCategories,callback: (s) =>{
+                                _selectedCategory = s
+                              },
+                            ),
+                            DropDown(this, label: "Contract",items: sampleContracts,callback: (s) =>{
+                                _selectedContract = s
+                              },
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(defaultPadding/2),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    decoration: const BoxDecoration(
+                                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                                      color: thirdColor5,
+                                    ),
+                                    child: DropdownSearch<String>.multiSelection(
+                                      items: assets,
+                                      mode: Responsive.isDesktop(context) ? Mode.MENU : Mode.BOTTOM_SHEET,
+                                      showSelectedItems: true,
+                                      popupBackgroundColor: thirdColor3,
+                                      dropdownSearchDecoration: const InputDecoration(
+                                        labelText: "Assets",
+                                        contentPadding: EdgeInsets.all(10),
+                                        border: InputBorder.none
+                                      ),
+                                      //popupItemDisabled: (String s) => s.startsWith('I'),
+                                      showSearchBox: true,
+                                      onSaved: (a){
+                                        selectedAssets = a;
+                                      },
+                                    ),
+                                  ),
+                                ]
+                              ),
+                            ),
+                            //TODO Still need a way to input estimated time
+                            ElevatedButton(
+                                onPressed: (){
+                                  //TODO Send the create ticket Request and Empty the page
+                                  print("Client: "+ _selectedClient +"\nTitle: " + ticketTitleController.text + "\nDescription: " + ticketDescriptionController.text + "\nCategory: " + _selectedCategory + "\nContract: " + _selectedContract);
+                                },
+                                child: const Text("Create Ticket"))
+                          ],
+                        ),
                       ),
-                    )
+                    ),
                   ),
-                ),
               ),
             ),
           ],
@@ -187,7 +256,7 @@ class TextBox extends StatelessWidget {
               child:TextFormField(
                 keyboardType: TextInputType.multiline,
                 minLines: 6,
-                maxLines: 6,
+                maxLines: 20,
                 controller: controller,
                 decoration: InputDecoration(
                     contentPadding: const EdgeInsets.only(left: 10,bottom: 8),
@@ -206,3 +275,51 @@ class TextBox extends StatelessWidget {
   }
 }
 
+
+class DropDown extends StatelessWidget {
+  const DropDown(_NewTicketScreenState _newTicketScreenState, {Key? key, required this.label, required this.items, required this.callback}) : super(key: key);
+
+  final String label;
+  final List<String> items;
+
+  final Function(String s) callback;
+
+  bool whatDropdownMode(BuildContext context){
+    return (MediaQuery.of(context).size.width > 800);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding/2),
+      child: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              //borderRadius: whatDropdownMode(context)? const BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)) : const BorderRadius.all(Radius.circular(10)),
+              color: thirdColor5,
+            ),
+            height: 50,
+            child: DropdownSearch<String>(
+              items: items,
+              mode:whatDropdownMode(context) ? Mode.MENU : Mode.BOTTOM_SHEET,
+              showSelectedItems: true,
+              popupBackgroundColor: thirdColor3,
+              dropdownSearchDecoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  border: InputBorder.none,
+                  labelText: label,
+              ),
+              //popupItemDisabled: (String s) => s.startsWith('I'),
+              showSearchBox: true,
+              onChanged: (s){
+                callback(s ?? "");
+              },
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
