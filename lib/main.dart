61723e49587d355bbfeb15d1e5b98dd33bf0c9fb
Fred <fredp.profissional@gmail.com>
2022-05-11 18:47:53 +0100

    fixed .env file

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'consts.dart';
 
 void main() async{
-  await dotenv.load(fileName: "../.env");
+  await dotenv.load();
   runApp(const SMA());
 }
 
